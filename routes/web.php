d8cd59c14722ac9f2ec6d735b149a16baea3ce26
Mostofa Kamal <mostofakamal3636@gmail.com>
2023-06-05 23:45:06 +0600

    rout

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,13 @@ Route::get('/', function () {
     ]);
 });
 
+
+
+
+
+
+
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
